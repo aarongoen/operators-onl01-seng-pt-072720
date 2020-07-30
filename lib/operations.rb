3d2960ee5677b228f31920f1918baea80541da26
Aaron Goen <aarongoen2.0@gmail.com>
2020-07-30 13:37:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed < 40 || speed > 60
+  if (speed < 40 || speed > 60)
     print "true"
   else
     puts "false"
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-	speed > 40 || speed < 60 ? "safe" : "not_safe"
+	(speed > 40 || speed < 60) ? "safe" : "not_safe"
 end
 	
 
